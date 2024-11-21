4f84e170b7e5d33f499533de0570e47bc75dbc39
clemcheyrou <clemcheyrou@gmail.com>
2024-11-21 18:25:20 +0100

    feat(p2): p2 is working

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -4,12 +4,14 @@ sudo apt-get install curl
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip $1 --write-kubeconfig-mode=644" sh -
 sleep 10
 
-kubectl create configmap app1-html --from-file '/vagrant/app1/app1.html'
+sudo chmod 777 /etc/hosts
+
+kubectl create configmap app1 --from-file '/vagrant/app1/index.html'
 kubectl apply -f '/vagrant/app1/app1.yaml'
 
-kubectl create configmap app2-html --from-file '/vagrant/app2/app2.html'
+kubectl create configmap app2 --from-file '/vagrant/app2/index.html'
 kubectl apply -f '/vagrant/app2/app2.yaml'
 
-kubectl create configmap app3-html --from-file '/vagrant/app3/app3.html'
+kubectl create configmap app3 --from-file '/vagrant/app3/index.html'
 kubectl apply -f '/vagrant/app3/app3.yaml'
 
