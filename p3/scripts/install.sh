a9ec5cb4925655edf786d4ff6437d9c8e575013f
clemcheyrou <clemcheyrou@gmail.com>
2024-11-27 15:26:47 +0100

    feat(kubectl): add installation new try

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -30,6 +30,12 @@ k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8443:443@loadb
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl wait pod \
+--all \
+--for=condition=Ready \
+--namespace=${ns}
+
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
 sleep 10
