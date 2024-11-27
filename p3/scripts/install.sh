cd823248700004d98ed7c20f911f2bb22cd6ca18
clemcheyrou <clemcheyrou@gmail.com>
2024-11-27 15:16:01 +0100

    feat(kubectl): add installation new try

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,18 +1,29 @@
 sudo apt-get update
 sudo apt-get install ca-certificates curl
 
-# install docker
-curl -fsSL https://get.docker.com/ | sh
+# install docker and kubectl
+if command -v kubectl &> /dev/null; then
+    echo "Kubectl installation found"
+else
+    curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+    sudo chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    kubectl version --client
+
+    sleep 10
+fi
+
+if command -v docker &> /dev/null; then
+    echo "Docker installation found"
+else
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+fi
+
 
 # install k3d
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d --version
-
-# install kubectl
-curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version --client
 
 # create a single node cluster
 k3d cluster create dev-cluster --port 8080:80@loadbalancer --port 8080:443@loadbalancer
