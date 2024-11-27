c28890c087775a66a7c2297d1a70533a8b965418
clemcheyrou <clemcheyrou@gmail.com>
2024-11-27 18:32:40 +0100

    feat(kubectl): change config

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -40,10 +40,9 @@ kubectl wait pod \
 kubectl -n argocd get pods
 echo "All pods are ready"
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-sleep 10
 
-argocd login
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
 # kubectl apply -f ../confs/argocd.yaml
 
