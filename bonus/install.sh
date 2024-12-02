e833533518be3eac4312e1446bee19266ce80ded
clemcheyrou <clemcheyrou@gmail.com>
2024-12-02 19:19:34 +0100

    wip bonus

---
+++ bonus/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install curl
+
+# https://docs.gitlab.com/charts/installation/tools.html
+# install kubectl
+if command -v kubectl &> /dev/null; then
+    echo "Kubectl installation found"
+else
+    curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+    sudo chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+    kubectl version --client
+
+    sleep 10
+fi
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
+  --timeout 600s \
+  --set global.hosts.domain=example.com \
+  --set global.hosts.externalIP=10.10.10.10 \
+  --set certmanager-issuer.email=me@example.com
