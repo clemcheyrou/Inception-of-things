f45dfffb5aab58d034e6c7cb63153a825dfd72df
clemcheyrou <clemcheyrou@gmail.com>
2024-12-03 19:03:58 +0100

    wip

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -27,7 +27,7 @@ curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 k3d --version
 
 # create a single node cluster
-k3d cluster create dev-cluster -p "8888:30080@agent:0" --agents 2
+k3d cluster create dev-cluster -p "8888:80" --agents 2
 
 kubectl create namespace dev
 kubectl create namespace argocd
@@ -52,4 +52,4 @@ kubectl apply -n argocd -f ../confs/ingress.yaml
 kubectl apply -n argocd -f ../confs/project.yaml
 kubectl apply -n argocd -f ../confs/argocd.yaml
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+# kubectl port-forward svc/argocd-server -n argocd 8080:443
