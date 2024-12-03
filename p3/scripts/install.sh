5e0b81706e7af8357bad746b4119ab28b66270b1
clemcheyrou <clemcheyrou@gmail.com>
2024-12-03 17:56:08 +0100

    wip

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -29,28 +29,28 @@ k3d --version
 # create a single node cluster
 k3d cluster create dev-cluster -p "8082:30080"
 
-kubectl create namespace dev
-kubectl create namespace argocd
+# kubectl create namespace dev
+# kubectl create namespace argocd
 
-kubectl apply -n dev -f '../dev/dev.yaml'
+kubectl apply -f '../dev/dev.yaml'
 
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sleep 10
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+# sleep 10
 
-kubectl wait pod \
---all \
---for=condition=Ready \
---namespace=argocd
+# kubectl wait pod \
+# --all \
+# --for=condition=Ready \
+# --namespace=argocd
 
-kubectl -n argocd get pods
-echo "All pods are ready"
+# kubectl -n argocd get pods
+# echo "All pods are ready"
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-echo ""
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+# echo ""
 
-kubectl apply -n argocd -f ../confs/ingress.yaml
-kubectl apply -n argocd -f ../confs/project.yaml
-kubectl apply -n argocd -f ../confs/argocd.yaml
+# kubectl apply -n argocd -f ../confs/ingress.yaml
+# kubectl apply -n argocd -f ../confs/project.yaml
+# kubectl apply -n argocd -f ../confs/argocd.yaml
 
 # kubectl port-forward svc/argocd-server -n argocd 8080:443
 # kubectl port-forward svc/dev-service 8888:80 -n dev
