d71fd82503bf245b2a433c943d664f38590ac4e1
clemcheyrou <clemcheyrou@gmail.com>
2024-12-04 11:37:56 +0100

    test

--- p3/scripts/argocd.sh
+++ p3/scripts/argocd.sh
@@ -19,4 +19,4 @@ kubectl apply -n argocd -f ../confs/ingress.yaml
 kubectl apply -n argocd -f ../confs/project.yaml
 kubectl apply -n argocd -f ../confs/argocd.yaml
 
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
