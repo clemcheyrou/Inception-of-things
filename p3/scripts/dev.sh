1f46798c5a91cd8f90a545778541c2c392823af7
clemcheyrou <clemcheyrou@gmail.com>
2024-12-04 11:27:57 +0100

    test

--- p3/scripts/dev.sh
+++ p3/scripts/dev.sh
@@ -2,6 +2,7 @@
 
 kubectl create namespace dev
 kubectl apply -f '../dev/dev.yaml'
+sleep 10
 
 kubectl wait pod \
 --all \
@@ -10,3 +11,5 @@ kubectl wait pod \
 
 kubectl -n dev get pods
 echo "All pods are ready"
+
+kubectl port-forward svc/dev-service -n dev 8080:8888
