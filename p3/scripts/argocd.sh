fd1cfb1b1438e11fd03189d432af19802d2c3372
atardif <atardif@atardif.atardif>
2024-12-12 19:27:31 +0100

    [finalization]

--- p3/scripts/argocd.sh
+++ p3/scripts/argocd.sh
@@ -6,17 +6,17 @@ sleep 10
 kubectl wait pod \
   --all \
   --for=condition=Ready \
-  --namespace=argocd
+  --namespace=argocd \
+  --timeout=600s
 
 kubectl -n argocd get pods
 echo "All pods are ready"
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d >argocd_passwd
 echo ""
 
-kubectl apply -n argocd -f ../confs/argocd/ingress.yaml
+#kubectl apply -n argocd -f ../confs/argocd/ingress.yaml
 kubectl apply -n argocd -f ../confs/argocd/project.yaml
 kubectl apply -n argocd -f ../confs/argocd/argocd.yaml
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 >/dev/null 2>&1 &
-
