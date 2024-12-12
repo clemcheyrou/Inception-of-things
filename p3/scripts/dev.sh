931160d02f2cb36e5e51563761ec4f3582eb7f09
atardif <atardif@atardif.atardif>
2024-12-12 14:01:13 +0100

    test p3

--- p3/scripts/dev.sh
+++ p3/scripts/dev.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 kubectl create namespace dev
-kubectl apply -f '../confs/dev/dev.yaml' -n dev
-sleep 10
-
-kubectl wait pod \
-  --all \
-  --for=condition=Ready \
-  --namespace=dev
-
-kubectl -n dev get pods
-echo "All pods are ready"
-
-kubectl port-forward svc/dev-service -n dev 8888:8888
-
+# kubectl apply -f '../confs/dev/dev.yaml' -n dev
+# sleep 10
+#
+# kubectl wait pod \
+#   --all \
+#   --for=condition=Ready \
+#   --namespace=dev
+#
+# kubectl -n dev get pods
+# echo "All pods are ready"
+#
+# kubectl port-forward svc/dev-service -n dev 8888:8888
